225bab5867d856f899739260b83cf313f87b031a
gagichce <gagichce@mcmaster.ca>
2014-03-28 20:28:17 -0400

    initial commit, still needs parser and other features

---
+++ GPS-Shield-v1.1/GPS-Shield-v1.1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware;
+using SecretLabs.NETMF.Hardware.Netduino;
+using System.IO.Ports;
+
+namespace GPS_Shield_v1._1
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            SerialPort gpsModule = new SerialPort(SerialPorts.COM2, 4800);
+
+            OutputPort gpsPower = new OutputPort(Pins.GPIO_PIN_D4, false);
+
+            gpsModule.Open();
+
+            while (true)
+            {
+                Debug.Print("lala");
+                getResponse(gpsModule);
+                Thread.Sleep(2000);
+            }
+
+        }
+
+        private static void getResponse(SerialPort serial)
+        {
+            String response = "";
+            while (serial.BytesToRead > 0)
+            {
+                byte[] buf = new byte[1];
+                serial.Read(buf, 0, 1);
+                // Line feed - 0x0A - marks end of data.
+                // Append each byte read until end of data.
+                if (buf[0] != 0x0A)
+                {
+                    response += (char)buf[0];
+                }
+                else
+                {
+                    Debug.Print(response);
+                    break;
+                }
+            }
+            Debug.Print(response);
+        }
+    }
+}
